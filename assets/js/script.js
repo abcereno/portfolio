d6edd35c52d90a411979ee3b30aa1e36c187293d
Anthony Bernard Cereno <cereno.abc@gmail.com>
2023-05-16 11:51:53 +0800

    updated portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
         for ( let i = 0; i < reveals.length; i++){
             var windowHeight = window.innerHeight;
             var revealTop = reveals[i].getBoundingClientRect().top;
-            var revealPoint = 100;
+            var revealPoint = 150;
 
             if(revealTop < windowHeight - revealPoint){
                 reveals[i].classList.add('active');
