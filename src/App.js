ce207c2e17d4420371bc4ea34e82d24103896d3c
Anthony <cereno.abc@gmail.com>
2024-02-20 12:28:03 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from "react-bootstrap";
 import Navibar from "./Common/Navibar/Navibar";
 import Footer from "./Common/Footer/Footer";
-import NotFound from "./Links/NotFound/NotFound";
 import Affiliate from "./Links/Affiliates/Affiliate";
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/affiliates' element={<Affiliate />} />
-            <Route path='/*' element={<NotFound />} />
           </Routes>
       </Container>
       <Footer/>
